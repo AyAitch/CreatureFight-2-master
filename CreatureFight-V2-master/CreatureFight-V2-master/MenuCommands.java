4ff29f2a55694a972de9a76a217f2e0495a952e0
AyAitch <43046903+AyAitch@users.noreply.github.com>
2018-11-23 13:30:34 -0600

    Creature Fight 2
    This is a Game where creatures fight each other

---
+++ CreatureFight-V2-master/CreatureFight-V2-master/MenuCommands.java
@@ -0,0 +1,8 @@
+import greenfoot.*;
+/**
+ * MenuCommands executes the creature and idx methods
+ */
+public interface MenuCommands  
+{
+    public void execute(int idx, Creature c);
+}
---
+++ CreatureFight-V2-master/CreatureFight-V2-master/SwitchCommands.java
@@ -0,0 +1,18 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class PlayerCommands here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SwitchCommands implements MenuCommands
+{
+    /**
+     *  execute executes the creature and idx methods
+     */
+    public void execute(int idx, Creature c)
+    {
+        //Will be coded later in the semester
+    }
+}
